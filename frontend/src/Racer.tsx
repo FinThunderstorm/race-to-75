04e274a21f3ad03ea77558c17210fe34957fec29
Arokanto <arokanto@github.com>
2026-09-07 23:45:51 +0300

    Animate character tears with reduced-motion support

--- frontend/src/Racer.tsx
+++ frontend/src/Racer.tsx
@@ -2,6 +2,13 @@ import racer from './assets/racer.png'
 
 export const Racer = () => (
   <div className="racer" aria-hidden="true">
-    <img src={racer} alt="" />
+    <div className="racer-sprite">
+      <div className="racer-base" style={{ backgroundImage: `url(${racer})` }} />
+      <div className="racer-body">
+        <img src={racer} alt="" />
+        <span className="racer-tear racer-tear--left" />
+        <span className="racer-tear racer-tear--right" />
+      </div>
+    </div>
   </div>
 )
