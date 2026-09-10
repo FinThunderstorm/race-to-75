f8f771815b0dc70c5b520eeeefe17893ad733d8b
Tuomas Arokanto <tuomas.arokanto@reaktor.com>
2026-09-10 12:49:06 +0300

    Replace ai character with Homer

--- frontend/src/Racer.tsx
+++ frontend/src/Racer.tsx
@@ -1,14 +1,7 @@
-import racer from './assets/racer.png'
+import racer from './assets/homer.png'
 
 export const Racer = () => (
   <div className="racer" aria-hidden="true">
-    <div className="racer-sprite">
-      <div className="racer-base" style={{ backgroundImage: `url(${racer})` }} />
-      <div className="racer-body">
-        <img src={racer} alt="" />
-        <span className="racer-tear racer-tear--left" />
-        <span className="racer-tear racer-tear--right" />
-      </div>
-    </div>
+    <img src={racer} alt="" />
   </div>
 )
